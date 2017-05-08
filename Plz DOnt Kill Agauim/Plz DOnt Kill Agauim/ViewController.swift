2348d82e7a7710a78ee040ceed4b8e742c545403
FollowYourDreamsDontDoMemes <ethanb@my.prideprepschool.org>
2017-05-08 13:13:13 -0700

    obey the chnge

---
+++ Plz DOnt Kill Agauim/Plz DOnt Kill Agauim/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Plz DOnt Kill Agauim
+//
+//  Created by Student on 5/8/17.
+//  Copyright © 2017 Anything. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBOutlet weak var SpicyMemes: UILabel!
+    @IBOutlet weak var DankMemes: UITextField!
+    @IBAction func chicken(_ sender: Any) {
+    }
+   
+    @IBAction func TV(_ sender: Any) {
+    }
+    @IBAction func Computer(_ sender: Any) {
+    }
+    @IBAction func ChickenNuggets(_ sender: Any) {
+    }
+    @IBAction func Steak(_ sender: Any) {
+    }
+    @IBAction func Memes(_ sender: Any) {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
