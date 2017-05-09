84f3efc0a5d5e331b4a6b7acc46d7f4480b23ee6
FollowYourDreamsDontDoMemes <ethanb@my.prideprepschool.org>
2017-05-09 12:55:13 -0700

    Add files via upload

---
+++ HW3 starter/HW3 starter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW3 starter
+//
+//  Created by Student on 5/9/17.
+//  Copyright © 2017 Anything. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
